3439b8dc028ba20c1c15d5c67350290e60423ed7
alexaldearroyo <alexaldearroyo@gmail.com>
2024-06-10 17:54:30 +0200

    Add cart button

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,15 +1,27 @@
+// src/components/Header.js
+
 import Link from 'next/link';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faShoppingCart } from '@fortawesome/free-solid-svg-icons';
 
 export default function Header() {
   return (
-    <header className="mb-8">
-      <h1 className="text-4xl font-bold mb-4">The Digital Amulet Vault</h1>
-      <Link href="/" className="ml-4" data-test-id="products-link">
-        <span className="text-blue-500 hover:underline">Products</span>
+    <header className="mb-8 flex justify-between items-center">
+      <div>
+        <h1 className="text-4xl font-bold mb-4">The Digital Amulet Vault</h1>
+        <Link href="/" className="ml-4" data-test-id="products-link">
+          <span className="text-blue-500 hover:underline">Products</span>
+        </Link>
+      </div>
+      <Link href="/cart" className="ml-4" data-test-id="cart-link">
+        <button className="bg-blue-500 text-white px-6 py-3 rounded hover:bg-blue-600 flex items-center justify-center">
+          <FontAwesomeIcon
+            icon={faShoppingCart}
+            className="text-white text-xl"
+          />{' '}
+          Cart
+        </button>
       </Link>
-      {/* <Link href="/products" className="ml-4" data-test-id="products-link">
-        <span className="text-blue-500 hover:underline">Products</span>
-      </Link> */}
     </header>
   );
 }
