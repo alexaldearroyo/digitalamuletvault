18aac8a7de9e77a25c768d4b415f86ad2522857a
alexaldearroyo <alexaldearroyo@gmail.com>
2024-06-13 17:38:55 +0200

    Improve overall Checkout Page design

--- src/app/checkout/page.js
+++ src/app/checkout/page.js
@@ -67,171 +67,197 @@ const CheckoutPage = () => {
     .toFixed(2);
 
   return (
-    <div className="container mx-auto p-6">
-      <Header />
-      <main className="mt-8">
-        <p className="text-lg text-gray-600 mb-6">Total: {totalPrice}</p>
+    <div className="min-h-screen py-6 sm:py-12">
+      <div className="max-w-screen-xl mx-auto px-4 sm:px-6 lg:px-8">
+        <Header />
+      </div>
+      <div className="flex items-start justify-center mt-0 sm:mt-2">
+        <main className="mt-8 w-full max-w-lg bg-white rounded-lg shadow-md">
+          <div className="p-4 sm:p-6 lg:p-8">
+            <p className="text-lg text-gray-600 mb-6">Total: {totalPrice}</p>
+            <hr className="my-4" /> {/* Separator line */}
+            <form onSubmit={handleSubmit} className="space-y-4">
+              <h2 className="text-xl font-semibold">Shipping Address</h2>
 
-        <form onSubmit={handleSubmit} className="space-y-4">
-          <div>
-            <label>
-              First Name:
-              <input
-                type="text"
-                name="firstName"
-                value={firstName}
-                data-test-id="checkout-first-name"
-                onChange={handleChange}
-                className="border-gray-300 rounded p-2 w-full"
-              />
-            </label>
+              <div className="grid grid-cols-2 gap-4">
+                <div>
+                  <label>
+                    First Name:
+                    <input
+                      type="text"
+                      name="firstName"
+                      value={firstName}
+                      placeholder="Enter First Name"
+                      data-test-id="checkout-first-name"
+                      onChange={handleChange}
+                      className="border border-gray-300 rounded-lg p-2 w-full"
+                    />
+                  </label>
+                </div>
+
+                <div>
+                  <label>
+                    Last Name:
+                    <input
+                      type="text"
+                      name="lastName"
+                      value={lastName}
+                      placeholder="Enter Last Name"
+                      data-test-id="checkout-last-name"
+                      onChange={handleChange}
+                      className="border border-gray-300 rounded-lg p-2 w-full"
+                    />
+                  </label>
+                </div>
+              </div>
+
+              <div>
+                <label>
+                  Email:
+                  <input
+                    type="email"
+                    name="email"
+                    value={email}
+                    placeholder="Enter Email"
+                    data-test-id="checkout-email"
+                    onChange={handleChange}
+                    className="border border-gray-300 rounded-lg p-2 w-full"
+                  />
+                </label>
+              </div>
+
+              <div>
+                <label>
+                  Address:
+                  <input
+                    type="text"
+                    name="address"
+                    value={address}
+                    placeholder="Enter Address"
+                    data-test-id="checkout-address"
+                    onChange={handleChange}
+                    className="border border-gray-300 rounded-lg p-2 w-full"
+                  />
+                </label>
+              </div>
+
+              <div>
+                <label>
+                  City:
+                  <input
+                    type="text"
+                    name="city"
+                    value={city}
+                    placeholder="Enter City"
+                    data-test-id="checkout-city"
+                    onChange={handleChange}
+                    className="border border-gray-300 rounded-lg p-2 w-full"
+                  />
+                </label>
+              </div>
+
+              <div className="grid grid-cols-2 gap-4">
+                <div>
+                  <label>
+                    Postal Code:
+                    <input
+                      type="text"
+                      name="postalCode"
+                      value={postalCode}
+                      placeholder="Enter Postal Code"
+                      data-test-id="checkout-postal-code"
+                      onChange={handleChange}
+                      className="border border-gray-300 rounded-lg p-2 w-full"
+                    />
+                  </label>
+                </div>
+
+                <div>
+                  <label>
+                    Country:
+                    <input
+                      type="text"
+                      name="country"
+                      value={country}
+                      placeholder="Enter Country"
+                      data-test-id="checkout-country"
+                      onChange={handleChange}
+                      className="border border-gray-300 rounded-lg p-2 w-full"
+                    />
+                  </label>
+                </div>
+              </div>
+
+              <h2 className="text-xl font-semibold">Payment Information</h2>
+
+              <div>
+                <label>
+                  Credit Card:
+                  <input
+                    type="text"
+                    name="creditCard"
+                    value={creditCard}
+                    placeholder="Enter Credit Card"
+                    data-test-id="checkout-credit-card"
+                    onChange={handleChange}
+                    className="border border-gray-300 rounded-lg p-2 w-full"
+                  />
+                </label>
+              </div>
+
+              <div className="grid grid-cols-2 gap-4 mb-8">
+                <div>
+                  <label>
+                    Expiration Date:
+                    <input
+                      type="text"
+                      name="expirationDate"
+                      value={expirationDate}
+                      placeholder="Enter Expiration Date"
+                      data-test-id="checkout-expiration-date"
+                      onChange={handleChange}
+                      className="border border-gray-300 rounded-lg p-2 w-full"
+                    />
+                  </label>
+                </div>
+
+                <div>
+                  <label>
+                    Security Code:
+                    <input
+                      type="text"
+                      name="securityCode"
+                      value={securityCode}
+                      placeholder="Enter Security Code"
+                      data-test-id="checkout-security-code"
+                      onChange={handleChange}
+                      className="border border-gray-300 rounded-lg p-2 w-full"
+                    />
+                  </label>
+                </div>
+              </div>
+
+              {error && <p className="text-red-500">{error}</p>}
+
+              <div className="flex justify-center space-x-4 pt-5 pb-4">
+                <button
+                  type="submit"
+                  data-test-id="checkout-confirm-order"
+                  className="bg-blue-500 text-white px-6 py-3 rounded-lg hover:bg-blue-600 transition duration-300"
+                >
+                  Confirm Order
+                </button>
+
+                <Link href="/">
+                  <span className="inline-block bg-blue-500 text-white px-6 py-3 rounded-lg hover:bg-blue-600 transition duration-300">
+                    Continue Shopping
+                  </span>
+                </Link>
+              </div>
+            </form>
           </div>
-
-          <div>
-            <label>
-              Last Name:
-              <input
-                type="text"
-                name="lastName"
-                value={lastName}
-                data-test-id="checkout-last-name"
-                onChange={handleChange}
-                className="border-gray-300 rounded p-2 w-full"
-              />
-            </label>
-          </div>
-
-          <div>
-            <label>
-              Email:
-              <input
-                type="email"
-                name="email"
-                value={email}
-                data-test-id="checkout-email"
-                onChange={handleChange}
-                className="border-gray-300 rounded p-2 w-full"
-              />
-            </label>
-          </div>
-
-          <div>
-            <label>
-              Address:
-              <input
-                type="text"
-                name="address"
-                value={address}
-                data-test-id="checkout-address"
-                onChange={handleChange}
-                className="border-gray-300 rounded p-2 w-full"
-              />
-            </label>
-          </div>
-
-          <div>
-            <label>
-              City:
-              <input
-                type="text"
-                name="city"
-                value={city}
-                data-test-id="checkout-city"
-                onChange={handleChange}
-                className="border-gray-300 rounded p-2 w-full"
-              />
-            </label>
-          </div>
-
-          <div>
-            <label>
-              Postal Code:
-              <input
-                type="text"
-                name="postalCode"
-                value={postalCode}
-                data-test-id="checkout-postal-code"
-                onChange={handleChange}
-                className="border-gray-300 rounded p-2 w-full"
-              />
-            </label>
-          </div>
-
-          <div>
-            <label>
-              Country:
-              <input
-                type="text"
-                name="country"
-                value={country}
-                data-test-id="checkout-country"
-                onChange={handleChange}
-                className="border-gray-300 rounded p-2 w-full"
-              />
-            </label>
-          </div>
-
-          <div>
-            <label>
-              Credit Card:
-              <input
-                type="text"
-                name="creditCard"
-                value={creditCard}
-                data-test-id="checkout-credit-card"
-                onChange={handleChange}
-                className="border-gray-300 rounded p-2 w-full"
-              />
-            </label>
-          </div>
-
-          <div>
-            <label>
-              Expiration Date:
-              <input
-                type="text"
-                name="expirationDate"
-                value={expirationDate}
-                data-test-id="checkout-expiration-date"
-                onChange={handleChange}
-                className="border-gray-300 rounded p-2 w-full"
-              />
-            </label>
-          </div>
-
-          <div>
-            <label>
-              Security Code:
-              <input
-                type="text"
-                name="securityCode"
-                value={securityCode}
-                data-test-id="checkout-security-code"
-                onChange={handleChange}
-                className="border-gray-300 rounded p-2 w-full"
-              />
-            </label>
-          </div>
-
-          {error && <p className="text-red-500">{error}</p>}
-
-          <div>
-            <button
-              type="submit"
-              data-test-id="checkout-confirm-order"
-              className="bg-blue-500 text-white px-6 py-3 rounded hover:bg-blue-600 transition duration-300"
-            >
-              Confirm Order
-            </button>
-          </div>
-        </form>
-
-        <Link href="/">
-          <span className="inline-block bg-blue-500 text-white px-6 py-3 rounded mt-4 hover:bg-blue-600 transition duration-300">
-            Continue Shopping
-          </span>
-        </Link>
-      </main>
+        </main>
+      </div>
     </div>
   );
 };
